f8c457ac626fe85af1572795cce913553d025b56
Stas Kaledin <staskaledin@gmail.com>
2017-07-04 17:21:28 +0300

    Fix tests

--- flask_restframework/tests/test_middlewares.py
+++ flask_restframework/tests/test_middlewares.py
@@ -1,16 +1,17 @@
 import base64
-from unittest.mock import patch, Mock
 
 import pytest
 from flask.wrappers import Response
 
 from flask.ext.restframework.authentication_backend import SimpleBasicAuth
 from flask.ext.restframework.middlewares import AuthenticationMiddleware
+from flask.ext.restframework.tests.compat import mock
+
 
 @pytest.mark.test_auth_middleware_with_basic_auth
 def test_auth_middleware_with_basic_auth():
 
-    am = AuthenticationMiddleware(Mock(
+    am = AuthenticationMiddleware(mock.Mock(
         config={
             "BASIC_AUTH_LOGIN": "1",
             "BASIC_AUTH_PASSWORD": "1"
@@ -19,19 +20,19 @@ def test_auth_middleware_with_basic_auth():
 
 
 
-    with patch.object(AuthenticationMiddleware, "get_view") as m:
-        m.return_value = Mock(
+    with mock.patch.object(AuthenticationMiddleware, "get_view") as m:
+        m.return_value = mock.Mock(
             authentication_backends=[SimpleBasicAuth]
         )
 
-        with patch("flask.globals.request") as m:
+        with mock.patch("flask.globals.request") as m:
             response = am.before_request()
         assert response.headers['WWW-Authenticate'] == 'Basic realm="User Visible Realm"'
         assert response.status_code == 401
 
 
-        with patch("flask.globals.request", Mock(
-            authorization=Mock(
+        with mock.patch("flask.globals.request", mock.Mock(
+            authorization=mock.Mock(
                 username="1",
                 password="2"
             )
@@ -40,8 +41,8 @@ def test_auth_middleware_with_basic_auth():
             assert response.headers['WWW-Authenticate'] == 'Basic realm="User Visible Realm"'
             assert response.status_code == 401
 
-        with patch("flask.globals.request", Mock(
-            authorization=Mock(
+        with mock.patch("flask.globals.request", mock.Mock(
+            authorization=mock.Mock(
                 username="1",
                 password="1"
             )
